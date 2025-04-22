b3d850479f305c61abe80580210869386017bc42
Karen Payne <paynekaren@comcast.net>
2025-04-22 07:08:36 -0700

    Update solution structure and add DataScripts.sql
    - Modified the solution file to include new project items for `DataScripts.sql` and `readme.md`.
    - Removed folder references for `scripts\` in `DapperSampleApp.csproj` and `IncrementSequenceDemos.csproj`.
    - Deleted `populate.sql` and `script.sql` files.
    - Updated the cryptographic key in `Secrets.cs`.
    - Added `DataScripts.sql` with table definitions and initial data inserts for `Customers`, `CustomerSequence`, `Example`, `Example1`, `Orders`, `SampleComputed`, and `SampleComputed1`.
    - Made minor documentation corrections in `StringExtensions.cs`.
    
    by Karen

--- UtilitiesLibrary/StringExtensions.cs
+++ UtilitiesLibrary/StringExtensions.cs
@@ -4,7 +4,7 @@ public static class StringExtensions
 {
 
     /// <summary>
-    /// Use to split on upper cased characters and separate with a single space.
+    /// Use to split on upper-cased characters and separate with a single space.
     /// </summary>
     public static string SplitCamelCase(this string sender) =>
         string.Join(" ", Regex.Matches(sender, @"([A-Z][a-z]+)")
